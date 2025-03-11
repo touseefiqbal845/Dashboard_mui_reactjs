ebab8bec9996e25e4d7cbfaf193aabf23d1914b9
Umairtouseef <umairtuseef41@gmail.com>
2025-03-11 22:02:04 +0500

    pagenotfound

--- src/pages/page-not-found.tsx
+++ src/pages/page-not-found.tsx
@@ -10,7 +10,7 @@ export default function Page() {
   return (
     <>
       <Helmet>
-        <title> {`404 page not found! | Error - ${CONFIG.appName}`}</title>
+        <title> {`404 page not foun! | Error - ${CONFIG.appName}`}</title>
       </Helmet>
 
       <NotFoundView />
