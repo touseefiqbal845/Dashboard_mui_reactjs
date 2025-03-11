119053e83ebbda83164d7974807934557a3004c3
Umairtouseef <umairtuseef41@gmail.com>
2025-03-11 21:47:34 +0500

    New initial commit

--- src/layouts/components/nav-upgrade.tsx
+++ src/layouts/components/nav-upgrade.tsx
@@ -25,7 +25,7 @@ export function NavUpgrade({ sx, ...other }: StackProps) {
           ),
         })}
       >
-        More featuresd?
+        More features?
       </Typography>
       <Typography variant="body2" sx={{ color: 'text.secondary', mt: 0.5 }}>
         {`From only `}
